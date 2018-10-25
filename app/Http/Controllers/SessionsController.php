3df8e180ee060710e0f5aaced1d94200ec30f72f
mayy <577026014@qq.com>
2018-10-25 11:18:13 +0800

    Finish login and logout

---
+++ app/Http/Controllers/SessionsController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class SessionsController extends Controller
+{
+    /**
+     * 获取登录表单
+     */
+    public function create()
+    {
+        return view('sessions.create');
+    }
+
+    /**
+     * 提交登录
+     * @param Request $request
+     * @return mixed
+     */
+    public function store(Request $request)
+    {
+        $login_info = $this->validate($request, [
+            'email' => 'required|email',
+            'password' => 'required'
+        ]);
+
+        if (Auth::attempt($login_info, $request->has('remember'))) {
+            //登录成功
+            session()->flash('success', '欢迎回来！');
+            return redirect()->route('users.show', [Auth::user()]);
+        } else {
+            //登录失败
+            session()->flash('danger', '抱歉，您的邮箱和密码不匹配！');
+            return redirect()->back();
+        }
+        return;
+    }
+
+    /**
+     * 退出登录
+     */
+    public function destroy()
+    {
+        Auth::logout();
+        session()->flash('success', '您已成功退出！');
+        return redirect('login');
+    }
+}
